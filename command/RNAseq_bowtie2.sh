2a02008a11f25c6baaae06ecac49d9276134fb11
Chaeyeon Kim <cxyz.kim@gmail.com>
2025-03-27 11:43:02 +0900

    Update RNAseq_bowtie2.sh

--- command/RNAseq_bowtie2.sh
+++ command/RNAseq_bowtie2.sh
@@ -7,5 +7,5 @@ ref=$3
 bash command/0_mkdir.sh $name 
 bash command/1_bowtie2.sh $name $thread $ref
 bash command/2_filtering.sh $name $thread 
-bash command/3_featureCounts.sh $name $ref
+bash command/3_featureCounts.sh $name $thread $ref
 Rscript command/4_colnames.R $name 
