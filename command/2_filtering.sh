798fc87b8516b96f034ba5a6253ebcdbc3b91baf
Chaeyeon Kim <cxyz.kim@gmail.com>
2025-03-27 12:35:22 +0900

    Update 2_filtering.sh

--- command/2_filtering.sh
+++ command/2_filtering.sh
@@ -5,16 +5,16 @@ thread=$2
 
 cd work
 
-# Sort bam by name
+# Sort BAM by read name
 samtools sort -@ ${thread} -n -o $name/${name}_sorted.bam -O BAM $name/${name}_raw.bam
 
-# Filter and map quality cut
-samtools view -@ ${thread} -h -q 30 -F 0x4 -F 0x400 $name/${name}_sorted.bam -o $name/${name}_sorted_Qcut_mapped.bam
+# Filter reads: MAPQ ≥ 20, remove unmapped
+samtools view -@ ${thread} -h -q 20 -F 0x4 $name/${name}_sorted.bam -o $name/${name}_sorted_Qcut_mapped.bam
 
 # Fix mate information
 samtools fixmate -@ ${thread} -m $name/${name}_sorted_Qcut_mapped.bam $name/${name}_sorted_Qcut_mapped_fixmate.bam
 
-# Sort bam by coordinate
+# Sort BAM by coordinate
 samtools sort -@ ${thread} $name/${name}_sorted_Qcut_mapped_fixmate.bam -o $name/${name}_sorted_Qcut_mapped_fixmate_sorted.bam
 
 # Remove duplicates
@@ -22,5 +22,6 @@ samtools markdup -@ ${thread} -r -s $name/${name}_sorted_Qcut_mapped_fixmate_sor
 
 cd ..
 
-fastqc -t ${thread} -o work/$name/QC  work/${name}/*.bam
+# Run quality control
+fastqc -t ${thread} -o work/$name/QC work/${name}/*.bam
 multiqc work/$name/QC/* -o work/$name/QC
