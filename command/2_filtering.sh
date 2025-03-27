83094bbd4106c07721dbe0a0d92ded47f06d1977
Chaeyeon Kim <cxyz.kim@gmail.com>
2025-03-27 13:58:58 +0900

    Update 2_filtering.sh

--- command/2_filtering.sh
+++ command/2_filtering.sh
@@ -9,17 +9,21 @@ cd work
 samtools sort -@ ${thread} -n -o $name/${name}_sorted.bam -O BAM $name/${name}_raw.bam
 
 # Filter reads: MAPQ ≥ 20, remove unmapped
-samtools view -@ ${thread} -h -q 20 -F 0x4 $name/${name}_sorted.bam -o $name/${name}_sorted_Qcut_mapped.bam
+samtools view -@ ${thread} -h -q 20 -F 0x4 -o $name/${name}_sorted_Qcut_mapped.bam $name/${name}_sorted.bam
 
 # Fix mate information
 samtools fixmate -@ ${thread} -m $name/${name}_sorted_Qcut_mapped.bam $name/${name}_sorted_Qcut_mapped_fixmate.bam
 
 # Sort BAM by coordinate
-samtools sort -@ ${thread} $name/${name}_sorted_Qcut_mapped_fixmate.bam -o $name/${name}_sorted_Qcut_mapped_fixmate_sorted.bam
+samtools sort -@ ${thread} -o $name/${name}_sorted_Qcut_mapped_fixmate_sorted.bam $name/${name}_sorted_Qcut_mapped_fixmate.bam
 
-# Remove duplicates
-samtools markdup -@ ${thread} -r -s $name/${name}_sorted_Qcut_mapped_fixmate_sorted.bam $name/${name}_final.bam
+# Mark duplicates (do not remove)
+samtools markdup -@ ${thread} -s $name/${name}_sorted_Qcut_mapped_fixmate_sorted.bam $name/${name}_markdup_flagged.bam
 
+# Remove marked duplicates using flag
+samtools view -@ ${thread} -h -F 0x400 -o $name/${name}_final.bam $name/${name}_markdup_flagged.bam
+
+rm $name/${name}_markdup_flagged.bam
 cd ..
 
 # Run quality control
